40a621800ccf998ae5da48ae6906bf5ec7664347
t.ayazbek <temirlanaazbek@gmail.com>
2024-05-03 15:02:29 +0500

    do coding kata

--- dart/dart_test/lib/kata.dart
+++ dart/dart_test/lib/kata.dart
@@ -1,7 +1,38 @@
-class Kata {
-  const Kata();
+class Solution {
+  const Solution();
 
-  int getAnswer() {
-    return 6 * 9;
+  static const Map<String, int> romanMap = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+  };
+
+  //* It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+  int toInteger(String? s) {
+    if (s == null) {
+      return 0;
+    }
+    if (s.length > 16) {
+      return 0;
+    }
+
+    int result = 0;
+    int preValue = 0;
+    final int length = s.length - 1;
+    for (int i = length; i >= 0; i--) {
+      int value = romanMap[s[i]]!;
+      if (value < preValue) {
+        result -= value;
+      } else {
+        result += value;
+      }
+      preValue = value;
+    }
+    return result;
   }
 }
--- dart/dart_test/test/kata_test.dart
+++ dart/dart_test/test/kata_test.dart
@@ -2,14 +2,108 @@ import 'package:dart_test/kata.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Kata', () {
-    // Arrange
-    const kata = Kata();
+  group('calculation of Roman number to int', () {
+    test('I', () {
+      // Arrange
+      const Solution solution = Solution();
 
-    // Act
-    final answer = kata.getAnswer();
+      // Act
+      final answer = solution.toInteger('I');
 
-    // Assert
-    expect(answer, 54);
+      // Assert
+      expect(answer, 1);
+    });
+    test('III', () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger('III');
+
+      // Assert
+      expect(answer, 3);
+    });
+    test('IV', () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger('IV');
+
+      // Assert
+      expect(answer, 4);
+    });
+
+    test('VI', () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger('VI');
+
+      // Assert
+      expect(answer, 6);
+    });
+    test('IX', () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger('IX');
+
+      // Assert
+      expect(answer, 9);
+    });
+    test('LVIII', () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger('LVIII');
+
+      // Assert
+      expect(answer, 58);
+    });
+    test('MCMXCIV', () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger('MCMXCIV');
+
+      // Assert
+      expect(answer, 1994);
+    });
+    test('', () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger('');
+
+      // Assert
+      expect(answer, 0);
+    });
+    test(null, () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger(null);
+
+      // Assert
+      expect(answer, 0);
+    });
+
+    test('IIIIIIIIIIIIIII', () {
+      // Arrange
+      const Solution solution = Solution();
+
+      // Act
+      final answer = solution.toInteger('IIIIIIIIIIIIIII');
+
+      // Assert
+      expect(answer, 15);
+    });
   });
 }
